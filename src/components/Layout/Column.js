000e87337e1903c0019709f3074c70782f4c1f0a
José Porto <joseporto@icloud.com>
2019-07-11 15:59:26 +0200

    feat: 🎸 make columns horizontal by default, add justify to column
    BREAKING CHANGE: columns contain justify attribute

--- src/components/Layout/Column.js
+++ src/components/Layout/Column.js
@@ -1,12 +1,10 @@
 const generateBreakpoints = (name, limit, condition) => {
   const result = {};
-  for(let i = 1; i <= limit; i++) {
+  for (let i = 1; i <= limit; i++) {
     result[`layout__column--${name}-${i}`] = condition && condition === i
   }
   return result
 }
-
-// import './column.scss';
 
 export default {
   name: 'Column',
@@ -40,6 +38,68 @@ export default {
           'baseline'
         ].indexOf(value) !== -1
       }
+    },
+    /**
+     * Horizontal column alignment
+     */
+    justify: {
+      type: String,
+      validator(value) {
+        return (
+          [
+            'start',
+            'end',
+            'center',
+            'between',
+            'around',
+            'evenly',
+            'initial',
+            'inheri'
+          ].indexOf(value) !== -1
+        );
+      }
+    },
+    /**
+     * Shortcut for `justify="start"`
+     */
+    left: {
+      type: Boolean,
+      default: false
+    },
+    /**
+     * Shortcut for `justify="center"`
+     */
+    center: {
+      type: Boolean,
+      default: false
+    },
+    /**
+     * Shortcut for `justify="end"`
+     */
+    right: {
+      type: Boolean,
+      default: false
+    },
+    /**
+      * Shortcut for `justify="between"`
+      */
+    between: {
+      type: Boolean,
+      default: false
+    },
+    /**
+     * Shortcut for `justify="around"`
+     */
+    around: {
+      type: Boolean,
+      default: false
+    },
+    /**
+     * Shortcut for `justify="evenly"`
+     */
+    evenly: {
+      type: Boolean,
+      default: false
     },
     xs: {
       type: Boolean | Number,
@@ -113,14 +173,22 @@ export default {
   computed: {
     classes() {
       const classes = {
-        'layout__column': true,
+        layout__column: true,
         'layout__row--reverse': this.reverse,
         'layout__column--align-stretch': this.align && this.align === 'strech',
         'layout__column--align-start': this.align && this.align === 'start',
         'layout__column--align-end': this.align && this.align === 'end',
         'layout__column--align-center': this.align && this.align === 'center',
         'layout__column--align-baseline': this.align && this.align === 'baseline',
-        'layout__column--xs': this.xs,// && !Number.isInteger(this.xs),
+        'layout__column--justify-start': (this.justify && this.justify === 'start') || this.left,
+        'layout__column--justify-end': (this.justify && this.justify === 'end') || this.right,
+        'layout__column--justify-center': (this.justify && this.justify === 'center') || this.center,
+        'layout__column--justify-between': (this.justify && this.justify === 'between') || this.between,
+        'layout__column--justify-around': (this.justify && this.justify === 'around') || this.around,
+        'layout__column--justify-evenly': (this.justify && this.justify === 'evenly') || this.evenly,
+        'layout__column--justify-initial': this.justify && this.justify === 'initial',
+        'layout__column--justify-inherit': this.justify && this.justify === 'inherit',
+        'layout__column--xs': this.xs,
         ...generateBreakpoints('xs', 12, this.xs),
         ...generateBreakpoints('sm', 12, this.sm),
         ...generateBreakpoints('md', 12, this.md),
@@ -135,22 +203,18 @@ export default {
         'layout__column--last': this.last,
         'layout__column--nogutter': this.nogutter,
         'layout__column--debug': this.debug
-      }
-      return classes
-    },
+      };
+      return classes;
+    }
   },
-  render (createElement) {
-    /* if (this.span) {
-      style.flex = `0 0 ${this.column}%`
-    } else {
-      style.left = 'auto',
-      style.right = 'auto'
-    } */
-
+  render(createElement) {
     return createElement(
-      this.tag, {
+      this.tag,
+      {
         attrs: this.$attrs,
         class: this.classes
-      }, this.$slots.default)
+      },
+      this.$slots.default
+    );
   }
-}
+};
